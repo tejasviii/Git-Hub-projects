43cf84cdf357ed06bc433224ba89d6bcb373c6e3
tejasviii <102219382+tejasviii@users.noreply.github.com>
2023-02-05 17:10:35 +0530

    Update response1.java

--- chuckNorisTejsavi/chucknoric-java/response1.java
+++ chuckNorisTejsavi/chucknoric-java/response1.java
@@ -1,18 +1,19 @@
-[
-    "animal",
-    "career",
-    "celebrity",
-    "dev",
-    "explicit",
-    "fashion",
-    "food",
-    "history",
-    "money",
-    "movie",
-    "music",
-    "political",
-    "religion",
-    "science",
-    "sport",
-    "travel"
-]
+package org.example;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+public class chuckJsonNoris {
+    public static void main(String[] args) throws Exception {
+        String url = "https://api.chucknorris.io/jokes/random";
+
+        HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create(url)).build();
+
+        HttpClient httpClient = HttpClient.newBuilder().build();
+
+        HttpResponse<String> httpResponse = httpClient.send(request,HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(httpResponse.statusCode());
+        System.out.println(httpResponse.body());
+    }
+}
